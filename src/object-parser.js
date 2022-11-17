73b8b98fbe398d5beec0d0b11d6cd594bda0bf6f
pansong291 <pansong291@sina.com>
2022-11-17 18:10:44 +0800

    refactor: 重构对象解析器，支持传入空目标

--- src/object-parser.js
+++ src/object-parser.js
@@ -1,12 +1,27 @@
 ;(function () {
     /**
+     * 字符串转义序列
+     *
+     * @type {{}}
+     */
+    const stringEscape = {
+        '0': '\0',
+        'b': '\b',
+        't': '\t',
+        'n': '\n',
+        'v': '\v',
+        'f': '\f',
+        'r': '\r'
+    }
+
+    /**
      * 根据指定的路径字符串获取指定对象中的值。
      *
-     * @param root 根对象或数组
+     * @param target 根对象或数组
      * @param pathStr 指定路径的字符串，必须以点号或方括号开头，如 .person.name 或 [0].name
-     * @returns {any} 指定对象中对应路径的值
+     * @returns {*} 指定对象中对应路径的值
      */
-    function getValue(root, pathStr) {
+    function getValue(target, pathStr) {
         // TODO
     }
 
@@ -14,95 +29,139 @@
      * 根据指定的字符串解析对应路径并将指定数据填充到指定对象中。
      * 指定的字符串格式不正确将会抛出异常。
      *
-     * @param root 需要填充数据的对象或数组
+     * @param target 需要填充数据的对象或数组，为空则自动识别
      * @param pathStr 指定路径的字符串，必须以点号或方括号开头，如 .person.name 或 [0].name
      * @param value 需要填充的数据
+     * @returns {*} 目标对象或数组
      */
-    function setValue(root, pathStr, value) {
-        const errMsg = `error: illegal path string: ${pathStr}`
-        const reg = /[\[.]/
-        let cur = root
+    function setValue(target, pathStr, value) {
         let path = pathStr
-        // 先查找一次
-        const matchArr = path.match(reg)
-        // 未找到或者不在首个位置抛异常
-        if (!matchArr || matchArr.index !== 0) {
-            throw errMsg
-        }
+        const root = {target}
+        let cur = root
+        let field = 'target'
         while (true) {
-            // 检查当前对象是否有值且是否是对象或数组类型
-            if (!cur || !(cur instanceof Object)) {
-                const deepPath = pathStr.substring(0, pathStr.lastIndexOf(path))
-                throw `error: the value of "<root>${deepPath}" is not a(n) object/array`
-            }
-            // 获取第一个符号
+            let parseResult = null
+            // 必须以 . 或 [ 开头
             const symbol = path.charAt(0)
-            // 去掉第一个符号
-            path = path.substring(1)
-            // 继续找下一个
-            const nextMatch = path.match(reg)
-            // 获取当前属性名和下个符号
-            let field
-            let nextSymbol
-            if (nextMatch) {
-                field = path.substring(0, nextMatch.index)
-                nextSymbol = nextMatch[0]
-            } else {
-                field = path
-                nextSymbol = null
+            if (symbol === '.') {
+                parseResult = parseDotMark(path)
+            } else if (symbol === '[') {
+                parseResult = parseSquareBrackets(path)
             }
-            if (symbol === '[') {
-                // 检查一下中括号是否配对
-                if (field.charAt(field.length - 1) !== ']') {
-                    throw errMsg
-                }
-                // 把属性名最后的 ] 去掉，顺便去掉引号
-                field = removeQuote(field.substring(0, field.length - 1))
+            if (!parseResult || parseResult.error) throw new Error(`illegal path string: ${pathStr}`)
+            // 解析一节路径后，保证上个路径是对象或数组
+            if (cur[field] === null || cur[field] === void 0) {
+                cur[field] = parseResult.value
+            } else if (!(cur[field] instanceof Object)) {
+                const deepPath = pathStr.substring(0, pathStr.lastIndexOf(path))
+                throw new Error(`the value of "<root>${deepPath}" is not a(n) object/array`)
             }
-            // 检查属性名不为空，且不能包含右方括号
-            if (!field || field.indexOf(']') >= 0) {
-                throw errMsg
-            }
-            if (nextSymbol === '.') {
-                // 点号表示是对象，需要保证当前路径是对象
-                if (cur[field] === void 0 || cur[field] === null) {
-                    cur[field] = {}
-                }
-            } else if (nextSymbol === '[') {
-                // 中括号表示是数组，需要保证当前路径是数组
-                if (cur[field] === void 0 || cur[field] === null) {
-                    cur[field] = []
-                }
-            } else {
-                // 未找到下个，结束
+            cur = cur[field]
+            field = parseResult.field
+            path = parseResult.nextPath
+            if (!path) {
                 cur[field] = value
                 break
             }
-            // 进入下一次迭代
-            cur = cur[field]
-            path = path.substring(nextMatch.index)
         }
+        return root.target
     }
 
     /**
-     * 如果字符串的首尾都是引号，则去除首尾的引号。检测的引号有单引号、双引号和反引号。
+     * 解析 . 开头的一节路径
      *
-     * @param str 受检字符串
-     * @returns {string} 去除首尾引号的字符串或它本身
+     * @param path
+     * @returns {{}}
      */
-    function removeQuote(str) {
-        let reg = /^(["'`]).*\1$/
-        if (reg.test(str)) {
-            str = str.substring(1, str.length - 1)
+    function parseDotMark(path) {
+        const result = {}
+        const reg = /^\.([$_a-zA-Z][$_a-zA-Z0-9]*)([\s\S]*?)$/
+        const matchArr = path.match(reg)
+        if (matchArr && matchArr.length && matchArr.length > 1) {
+            result.field = matchArr[1]
+            result.nextPath = matchArr[2]
+            result.value = {}
+        } else {
+            result.error = true
         }
-        return str
+        return result
     }
 
-    function parsePathOf(root, pathStr, value) {
-        if (value === void 0) {
-            return getValue(root, pathStr)
+    /**
+     * 解析 [ 开头的一节路径
+     *
+     * @param path
+     * @returns {{}}
+     */
+    function parseSquareBrackets(path) {
+        const result = {}
+        if (path.length > 2) {
+            const quote = path.charAt(1)
+            if (quote === "'" || quote === '"' || quote === '`') {
+                let field = ''
+                let escape = -1
+                let i = 2
+                stringLoop: for (; i < path.length; i++) {
+                    const ch = path.charAt(i)
+                    switch (ch) {
+                        case '\n':
+                            if (quote === '`') {
+                                field += ch
+                            } else {
+                                result.error = true
+                                break stringLoop
+                            }
+                            break
+                        case '\\':
+                            if (escape === i) {
+                                field += ch
+                            } else {
+                                escape = i + 1
+                            }
+                            break
+                        case quote:
+                            if (escape !== i) {
+                                break stringLoop
+                            }
+                            field += ch
+                            break
+                        default:
+                            if (escape === i) {
+                                field += stringEscape[ch] || ch
+                            } else {
+                                field += ch
+                            }
+                    }
+                }
+                if (!result.error) {
+                    // 没找到结尾引号或结尾引号后面的字符不配对方括号
+                    result.error = i === path.length || path.charAt(i + 1) !== ']'
+                    result.field = field
+                    result.nextPath = path.substring(i + 2)
+                    result.value = {}
+                }
+            } else {
+                const reg = /^\[([-+]?\d+)]([\s\S]*?)$/
+                const matchArr = path.match(reg)
+                if (matchArr && matchArr.length && matchArr.length > 1) {
+                    result.field = Number(matchArr[1])
+                    result.nextPath = matchArr[2]
+                    result.value = []
+                } else {
+                    result.error = true
+                }
+            }
         } else {
-            setValue(root, pathStr, value)
+            result.error = true
+        }
+        return result
+    }
+
+    function parsePathOf(target, pathStr, value) {
+        if (value === void 0) {
+            return getValue(target, pathStr)
+        } else {
+            return setValue(target, pathStr, value)
         }
     }
 
@@ -110,4 +169,4 @@
     window.paso.objectParser = {
         parsePathOf, getValue, setValue
     }
-})()
+})();
