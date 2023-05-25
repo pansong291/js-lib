89eab85c17bb29699230bebd1ce6dad4d48bef46
pansong291 <pansong291@sina.com>
2023-05-25 14:49:21 +0800

    perf: 重定义 .flex 去除 .gap-2

--- src/popup-inject.js
+++ src/popup-inject.js
@@ -5,119 +5,119 @@
     const $ = window.jQuery
     let $doc = $(document)
     $(document.head).append(`
-            <style>
-                .${config.namespace} {
-                    color: black;
-                }
-                .${config.namespace} * {
-                    box-sizing: border-box;
-                }
-                .${config.namespace} .flex {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                }
-                .${config.namespace} .sticky-bar {
-                    position: fixed;
-                    top: ${config.location};
-                    left: 0;
-                    transform: translateX(calc(12px - ${config.collapse}));
-                    z-index: 99999999;
-                    background: #3D7FFF;
-                    color: white;
-                    padding: 2px 10px 2px 4px;
-                    cursor: pointer;
-                    user-select: none;
-                    border-radius: 0 12px 12px 0;
-                    transition: transform 0.5s ease;
-                }
-                .${config.namespace} .sticky-bar:hover {
-                    transform: none;
-                }
-                .${config.namespace} .mask {
-                    height: 100vh;
-                    width: 100vw;
-                    position: fixed;
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    z-index: 99999999;
-                    background: rgba(0,0,0,0.4);
-                    display: none;
-                    align-items: center;
-                    justify-content: center;
-                }
-                .${config.namespace} .popup {
-                    padding: 16px;
-                    background: #f0f2f5;
-                    border-radius: 2px;
-                }
-                .${config.namespace} label {
-                    margin-top: .5em;
-                    margin-bottom: 0;
-                    user-select: none;
-                }
-                .${config.namespace} .monospace {
-                    font-family: v-mono, Consolas, SFMono-Regular, Menlo, Courier, v-sans, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif, monospace, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-                }
-                .${config.namespace} .gap-2 {
-                    gap: 2px;
-                }
-                .${config.namespace} .button {
-                    user-select: none;
-                    min-width: unset;
-                    min-height: unset;
-                    margin: unset;
-                    padding: 4px 16px;
-                    color: #fff;
-                    border: 1px solid #3D7FFF;
-                    border-radius: 2px;
-                    background: #3D7FFF;
-                    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
-                    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.05);
-                }
-                .${config.namespace} textarea {
-                    background: white;
-                    resize: vertical;
-                }
-                .${config.namespace} .input, .${config.namespace} .button {
-                    height: 32px;
-                    transition: all 0.3s, height 0s;
-                }
-                .${config.namespace} .button:before, .${config.namespace} .button:after {
-                    content: none;
-                }
-                .${config.namespace} .button:hover, .${config.namespace} .button:focus {
-                    border-color: #669eff;
-                    background: #669eff;
-                }
-                .${config.namespace} .button:active {
-                    border-color: #295ed9;
-                    background: #295ed9;
-                }
-                .${config.namespace} .input {
-                    padding: 4px 8px;
-                    background: white;
-                    border: 1px solid #d9d9d9;
-                    border-radius: 2px;
-                }
-                .${config.namespace} .input:hover {
-                    border-color: #669eff;
-                    outline: 0;
-                }
-                .${config.namespace} .input:focus {
-                    border-color: #669eff;
-                    box-shadow: 0 0 0 2px rgba(61, 127, 255, 0.2);
-                    border-right-width: 1px;
-                    outline: 0;
-                }
-                ${config.style.replaceAll(/<\/?style>/g, '')}
-            </style>`)
+      <style>
+        .${config.namespace} {
+            color: black;
+        }
+        .${config.namespace} * {
+            box-sizing: border-box;
+        }
+        .${config.namespace} .flex {
+            display: flex;
+            flex-direction: row;
+            align-items: stretch;
+            justify-content: flex-start;
+        }
+        .${config.namespace} .flex.col {
+            flex-direction: column;
+        }
+        .${config.namespace} .sticky-bar {
+            position: fixed;
+            top: ${config.location};
+            left: 0;
+            transform: translateX(calc(12px - ${config.collapse}));
+            z-index: 99999999;
+            background: #3D7FFF;
+            color: white;
+            padding: 2px 10px 2px 4px;
+            cursor: pointer;
+            user-select: none;
+            border-radius: 0 12px 12px 0;
+            transition: transform 0.5s ease;
+        }
+        .${config.namespace} .sticky-bar:hover {
+            transform: none;
+        }
+        .${config.namespace} .mask {
+            height: 100vh;
+            width: 100vw;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            z-index: 99999999;
+            background: rgba(0,0,0,0.4);
+            display: none;
+            align-items: center;
+            justify-content: center;
+        }
+        .${config.namespace} .popup {
+            padding: 16px;
+            background: #f0f2f5;
+            border-radius: 2px;
+        }
+        .${config.namespace} label {
+            margin-top: .5em;
+            margin-bottom: 0;
+            user-select: none;
+        }
+        .${config.namespace} .monospace {
+            font-family: v-mono, Consolas, SFMono-Regular, Menlo, Courier, v-sans, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif, monospace, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        }
+        .${config.namespace} .button {
+            user-select: none;
+            min-width: unset;
+            min-height: unset;
+            margin: unset;
+            padding: 4px 16px;
+            color: #fff;
+            border: 1px solid #3D7FFF;
+            border-radius: 2px;
+            background: #3D7FFF;
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
+            box-shadow: 0 2px 0 rgba(0, 0, 0, 0.05);
+        }
+        .${config.namespace} textarea {
+            background: white;
+            resize: vertical;
+        }
+        .${config.namespace} .input, .${config.namespace} .button {
+            height: 32px;
+            transition: all 0.3s, height 0s;
+        }
+        .${config.namespace} .button:before, .${config.namespace} .button:after {
+            content: none;
+        }
+        .${config.namespace} .button:hover, .${config.namespace} .button:focus {
+            border-color: #669eff;
+            background: #669eff;
+        }
+        .${config.namespace} .button:active {
+            border-color: #295ed9;
+            background: #295ed9;
+        }
+        .${config.namespace} .input {
+            padding: 4px 8px;
+            background: white;
+            border: 1px solid #d9d9d9;
+            border-radius: 2px;
+        }
+        .${config.namespace} .input:hover {
+            border-color: #669eff;
+            outline: 0;
+        }
+        .${config.namespace} .input:focus {
+            border-color: #669eff;
+            box-shadow: 0 0 0 2px rgba(61, 127, 255, 0.2);
+            border-right-width: 1px;
+            outline: 0;
+        }
+        ${config.style.replaceAll(/<\/?style>/g, '')}
+      </style>`)
     const $container = $(`<div class="${config.namespace}"/>`)
     const $stickyBar = $(`<div class="sticky-bar">${config.actionName}</div>`)
     const $mask = $(`<div class="mask"/>`)
-    const $popup = $(`<div class="popup flex gap-2">${config.content}</div>`)
+    const $popup = $(`<div class="popup flex col">${config.content}</div>`)
     $container.append($stickyBar).append($mask)
     $mask.append($popup)
 
